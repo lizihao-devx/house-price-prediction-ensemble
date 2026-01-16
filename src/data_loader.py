2e472f585c767bd216b7b818301dd4777a4bd091
Zane Li <zaneli01@qq.com>
2026-01-17 00:26:53 +0800

    更新部分代码

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,8 +5,13 @@ class HousingDataLoader:
     def __init__(self, filepath):
         self.filepath = filepath
 
-    def load_clear(self)
+    def load_clear(self):
         # 加载数据
         df = pd.read_csv(self.filepath)
 
         # 过滤数据
+        df['面积(m²)'] = pd.to_numeric(df['面积(m²)'], errors='coerce') # errors='coerce' 会把无法转换的错位数据变成 NaN
+        df['价格(万元)'] = pd.to_numeric(df['价格(万元)'], errors='coerce')
+        df = df.dropna(subset=['面积(m²)', '价格(万元)'])
+
+        # 提取特征，正则解析户型
